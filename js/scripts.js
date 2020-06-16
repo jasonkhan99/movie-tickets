4324bd93eb013285184c3e13c1d7932a26dac55b
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-06-16 16:01:55 -0700

    add js function for price
    co-authored-by: Teresa Rosinski <trosinski89@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 //Business Logic -- shoot out an objec that inludes the price of the ticket, name of moive, senior or not, matinee or not
-function Ticket () {
-  this.ticketDetails= [];
+function price (senior, matinee) {
+  let price = 10;
+  if (senior === true || matinee === true){
+    price -=2;
+  } else if (senior === true  && matinee === true){
+    price -= 4;
+  } else {
+    price += 0;
+  }
+
+}
+
+function Ticket (movieName,seniorOrNot,matineeOrNot ) {
+  
 }
 
 
